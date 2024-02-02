b4a642a9e7da15d999097e4bd8ce8418719364c6
Kilabarus <kilabarus@gmail.com>
2024-02-02 16:16:59 +0300

    Create project, add files with XML data example and DB creation script

---
+++ DB_Creation_Script.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE OnlineShop_DB;
+USE OnlineShop_DB;
+
+CREATE TABLE Customer (
+	Customer_ID  INT 		   CONSTRAINT PK_Customer_ID PRIMARY KEY IDENTITY,
+	FullName 	 NVARCHAR(80)  NOT NULL,
+	EMail 		 VARCHAR(255)  CONSTRAINT UQ_Customer_Email UNIQUE
+);
+
+CREATE TABLE Purchase (
+	Purchase_ID		   INT	  CONSTRAINT PK_Purchase_ID PRIMARY KEY,
+	Customer_ID		   INT	  CONSTRAINT FK_Customer_ID FOREIGN KEY REFERENCES Customer (Customer_ID),
+	Registration_Date  DATE	  NOT NULL,
+	Total_Sum		   MONEY  NOT NULL
+);
+
+CREATE TABLE Product (
+	Product_ID  INT 		   CONSTRAINT PK_Product_ID PRIMARY KEY IDENTITY,
+	Title 		NVARCHAR(255)  NOT NULL,
+	Price 		MONEY		   NOT NULL
+);
+
+CREATE TABLE Purchase_Product (
+	Purchase_ID  INT  CONSTRAINT FK_Purchase_ID FOREIGN KEY REFERENCES Purchase (Purchase_ID),
+	Product_ID 	 INT  CONSTRAINT FK_Product_ID FOREIGN KEY REFERENCES Product (Product_ID),
+	Quantity 	 INT  CONSTRAINT CK_Quantity_Greater_Than_0 CHECK (Quantity > 0) NOT NULL,
+		
+	CONSTRAINT PK_Purchase_Product_ID PRIMARY KEY (Purchase_ID, Product_ID)
+);
